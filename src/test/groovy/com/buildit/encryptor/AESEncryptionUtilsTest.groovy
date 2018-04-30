7269fff1ce7d7fd5b74f11a0a51c9580f32ec50b
Tim Webster <tim.webster@gmail.com>
2018-04-30 16:46:35 +0100

    Fix incomplete test (#4)
    Fix incomplete test

--- src/test/groovy/com/buildit/encryptor/AESEncryptionUtilsTest.groovy
+++ src/test/groovy/com/buildit/encryptor/AESEncryptionUtilsTest.groovy
@@ -60,7 +60,10 @@ class AESEncryptionUtilsTest {
         AESEncryptionUtils.metaClass.static.isJceInstalled = {
             return false
         }
-        AESEncryptionUtils.encrypt(AES_128_KEY, PASSWORD)
+        String encrypted = AESEncryptionUtils.encrypt(AES_128_KEY, PASSWORD)
+        String decrypted = AESEncryptionUtils.decrypt(AES_128_KEY, encrypted)
+
+        assertThat(decrypted as String).isEqualTo(PASSWORD)
     }
 
     @Test(expected = InvalidKeyException.class)
